ec2efd8420536edaa22269c946fd3c01ce52881f
Sander Spaas <sander.spaas@gmail.com>
2021-12-01 10:46:57 +0100

    started working on main project structure

---
+++ Blok1/BasicGui/StartGui/Form1.cs
@@ -0,0 +1,10 @@
+namespace StartGui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Spyfall/Spyfall/Globals/ZipInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Spyfall.Globals
+{
+//    public struct SpelSessie : IComparable<SpelSessie>
+//    {
+//        public string Locatie { get; }
+//        public Array Rollen { get; }
+
+//        public SpelSessie(string locatie, Array rollen)
+//        {
+//            Locatie = locatie;
+//            Rollen = rollen;
+//        }
+
+//        //public override string ToString()
+//        //{
+//        //    return $"{Zipcode} - {Gemeente} ({Provincie})";
+//        //}
+
+//        //public int CompareTo(ZipInfo other)
+//        //{
+//        //    return Gemeente.CompareTo(other.Gemeente);
+//        //}
+//    }
+}
