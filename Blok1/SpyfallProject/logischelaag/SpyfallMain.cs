e893a7b1d24beaed9cd7eceea0d86c2aaf768412
Sander Spaas <sander.spaas@gmail.com>
2021-12-08 22:26:58 +0100

    added more exceptions to the getters and setter of spyfallmain
    added more exceptions to the getters and setter of spyfallmain to prevent the entry of wrong data

--- Blok1/SpyfallProject/logischelaag/SpyfallMain.cs
+++ Blok1/SpyfallProject/logischelaag/SpyfallMain.cs
@@ -14,11 +14,11 @@ namespace SpyfallProject.logischelaag
         {
         }
 
-        public int Aantalspelers { get => _aantalspelers; set => _aantalspelers = value; }
-        public int Aantalspionnen { get => _aantalspionnen; set => _aantalspionnen = value; }
-        public string Locatie { get => _locatie; set => _locatie = value; }
-        public ArrayList SpelerList { get => _spelerList; set => _spelerList = value; }
-        public string FilePath { get => _filePath; set => _filePath = value; }
+        public int Aantalspelers { get => _aantalspelers; set => _aantalspelers = (value < 10 && value > 4) ? value : throw new ArgumentException("Aantal spionnen mag niet meer dan 10 en niet minder dan 4 zijn"); }
+        public int Aantalspionnen { get => _aantalspionnen; set => _aantalspionnen = (value != 0) ? value : throw new ArgumentException("Aantal spionnen mag niet 0 zijn"); }
+        public string Locatie { get => _locatie; set => _locatie = (!string.IsNullOrWhiteSpace(value)) ? value : throw new ArgumentException("Locatie mag niet leeg zijn"); }
+        public ArrayList SpelerList { get => _spelerList; set => _spelerList = value ?? throw new ArgumentException("Spelerlijst mag niet leeg zijn"); }
+        public string FilePath { get => _filePath; set => _filePath = (!string.IsNullOrWhiteSpace(value)) ? value : throw new ArgumentException("FilePath mag niet leeg zijn"); }
 
         public void Voegtoe(Speler speler)
         {
