9798e8883a0b095c9f5835a61b26de9a9934792b
Sander Spaas <sander.spaas@gmail.com>
2021-12-14 17:15:57 +0100

    Started working on Cyclon!
    Used the project structure from the teacher and started trying out stuff for the project.

---
+++ Blok2/Cyclon/PresentationLayer/MainForm.cs
@@ -0,0 +1,15 @@
+﻿using Globals.Interfaces;
+
+namespace PresentationLayer
+{
+    public partial class MainForm : Form
+    {
+        private readonly ILogic _logic;
+        public MainForm(ILogic logic)
+        {
+            _logic = logic;
+            InitializeComponent();
+
+        }
+    }
+}
